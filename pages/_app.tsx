ca0d41758ba06609ea94eb0d10b883e5de167457
kotaesaki <jmkamy552@gmail.com>
2022-12-30 12:25:39 +0900

    eslintとprettierの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+const colors = {
+  brand: {
+    primary: "#1DA1F2",
+    secondary: "#F07167",
+    foundation: "#F7F7F8",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
